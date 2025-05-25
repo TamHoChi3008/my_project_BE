8fab96eef6b382c62c358b9fec905fb7b7fdbb8d
HOCHITAM <tam.hochi.r2@gmail.com>
2025-05-25 12:30:10 +0700

    #1 24052025_create_docker

---
+++ .dockerbuild/web/launchWeb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+composer install --ignore-platform-reqs
+
+composer global require "laravel/installer:^5.0"
+
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
+. .env
+
+
+if [ -z "$APP_KEY" ]; then
+    php artisan key:generate;
+fi
+
+if [ -n "$DB_HOST" -a -n "$DB_DATABASE" -a -n "$DB_USERNAME" -a -n "$DB_PASSWORD" ]; then
+    php artisan migrate;
+else
+    echo "any db parameters are empty."
+fi
+
+# CentOS/Amazon Linux:
+# httpd -DFOREGROUND
+# Debian/Ubuntu base:
+apache2-foreground
